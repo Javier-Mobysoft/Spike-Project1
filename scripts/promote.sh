22882f38a4581dd496e8c8458e1588d4d1061c85
Javier <javier.gomez@mobysoft.com>
2017-01-12 15:54:53 +0000

    Using dependencies properties

--- scripts/promote.sh
+++ scripts/promote.sh
@@ -2,4 +2,5 @@
 commit_date=`git show -s --format="%ct"`
 formatted_date=`date -d @${commit_date} +"%Y%m%d%H%M"`
 incremental_version=`echo ${GO_PIPELINE_LABEL} | awk '{split($0,a,"."); print a[2]}'`
+mvn versions:update-parent -DallowSnapshots=false
 mvn versions:set -DnewVersion=${formatted_date}-${incremental_version} 
