4a613f9e56616a66d933503fbd113f3862e70bef
Javier <javier.gomez@mobysoft.com>
2017-01-12 16:12:41 +0000

    Versioning

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 commit_date=`git show -s --format="%ct"`
 formatted_date=`date -d @${commit_date} +"%Y%m%d%H%M"`
-incremental_version=`echo ${GO_PIPELINE_LABEL} | awk '{split($0,a,"."); print a[2]}'`
+incremental_version=`echo ${GO_PIPELINE_LABEL} | awk '{split($0,a,"."); print a[3]}'`
 mvn versions:update-parent -DallowSnapshots=false
 mvn versions:set -DnewVersion=${formatted_date}-${incremental_version}-SNAPSHOT  
